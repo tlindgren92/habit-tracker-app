32d5579c11986eea41c574cc155884e5b7281b26
Orewakkun <keshavchaudhary391@gmail.com>
2026-03-17 22:02:27 +0530

    Day 4: Add delete button,fix event bubbling

--- script.js
+++ script.js
@@ -14,9 +14,44 @@ addButton.addEventListener("click",()=>{
         inputClass.value="";
         return
     };
-    //creating the value using js
-    let newHabitHTML=`<div>${inputClassValue}</div> `;
-    habitContainer.innerHTML+=newHabitHTML;
+    //creating the value using js 
+    // let newHabitHTML=`<div>${inputClassValue}</div> `;
+
+    // habitContainer.innerHTML+=newHabitHTML;
+    // inputClass.value="";
+    
+
+    //using createElement method
+    let newElement=document.createElement("div");
+    let text=document.createElement("span");
+    let deleteButton=document.createElement("button");
+
+    //inserted the value
+    text.textContent=inputClassValue;
+    deleteButton.textContent="delete";
+
+    //append into habitcontainer
+    newElement.appendChild(text);
+    newElement.appendChild(deleteButton);
+    habitContainer.appendChild(newElement);
+
+    //when clicked,it will add class (complete) in the div
+    //when again clicked, it will remove the class(complete ) from the div
+    newElement.addEventListener("click",()=>{
+        // console.log("complete fuction run");
+        newElement.classList.toggle("completed");
+    })
+
+    //delete button funtion, click=remove that habit
+    //event bubbling happen here
+    deleteButton.addEventListener("click",(e)=>{        
+        e.stopPropagation();
+        deleteButton.parentElement.remove();              
+    })
+
+    //update the value inside input class after append
     inputClass.value="";
+
+    
     
 });
